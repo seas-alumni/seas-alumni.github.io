d9d3efca3c8b832aeebdc2eb3891d13b1df26347
Aditya Parikh <Aditya Parikh>
2017-05-04 18:34:19 +0530

    Initial commit

--- contact_form.js
+++ contact_form.js
@@ -1,26 +1,22 @@
-$(document).ready(function() {
-        $("#submit").click(function() {
-        var name = $("#name").val();
-        var email = $("#email").val();
-        var message = $("#message").val();
-        // var contact = $("#contact").val();
-        $("#returnmessage").empty(); // To empty previous error/success message.
-        // Checking for blank fields.
-        if (name == '' || email == '' ) {
-        alert("Please Fill Required Fields");
-        } else {
-        // Returns successful data submission message when the entered information is stored in database.
-        $.post("contact_form.php", {
-        name1: name,
-        email1: email,
-        message1: message,
-        // contact1: contact
-        }, function(data) {
-        $("#returnmessage").append(data); // Append returned message to message paragraph.
-        if (data == "Your Query has been received, We will contact you soon.") {
-        $("#form")[0].reset(); // To reset form fields on success.
-        }
-        });
-        }
-        });
-        });
+var message = "SEAS ALUMNI ASSOCIATION";
+var msgCount = 0;
+var blinkCount = 0;
+var blinkFlg = 0;
+var timer1, timer2;
+var messageLabel = document.getElementById("messageLabel");
+
+function textFunc() {
+   messageLabel.innerHTML = message.substring(0, msgCount);
+   
+   if (msgCount == message.length) {
+      // Stop Timer
+      clearInterval(timer1);
+      // msgCount=0;
+      
+   } else {
+      msgCount++;
+   }
+}
+
+
+timer1 = setInterval("textFunc()", 50); // Every 50 milliseconds
